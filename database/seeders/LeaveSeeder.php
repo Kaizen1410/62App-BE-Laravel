2acb93df224027787c85bac116b26a2c0b474c20
Kaizen1410 <lutfifahrezi04@gmail.com>
2023-09-12 16:02:10 +0700

    leave seeder & auth command

--- database/seeders/LeaveSeeder.php
+++ database/seeders/LeaveSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Leave;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class LeaveSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Leave::insert([
+            [
+                'employee_id' => 2,
+                'date_leave' => '2023-09-11',
+                'is_approved' => false,
+            ]
+            ]);
     }
 }
