7402b014a14a5659594522365df88143033af5e8
Ahmad Muhajir <amuhajir.syamlan@gmail.com>
2023-09-22 15:33:01 +0700

    change auth guard

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,12 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        //API Sanctum Guards
+        'admin-api' => [
+            'driver' => 'sanctum',
+            'provider' => 'users',
+        ]
     ],
 
     /*
